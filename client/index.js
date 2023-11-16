5f5d5067a6e76fe592c21bab80bf7d6669c7f403
unknown <aethagoan@outlook.com>
2023-11-16 15:25:12 -0700

    11/16/23, 3:25

--- client/index.js
+++ client/index.js
@@ -1,7 +1,11 @@
 
+// IMPORTANT: CHANGE THIS IS YOU REDO THE WEB APP!
+const API_PORT = "5184"
+
+
 // request something from localhost
 let api_message = await fetch(
-    "http://localhost:5184", 
+    "http://localhost:" + API_PORT, 
     {
     method: "GET"
     }
@@ -16,7 +20,7 @@ console.log(api_message)
 
 // request something from localhost
 api_message = await fetch(
-    "http://localhost:5184", {
+    "http://localhost:" + API_PORT, {
         method: "POST",
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -34,7 +38,7 @@ console.log(api_message)
 
 // request something from localhost
 api_message = await fetch(
-    "http://localhost:5184", 
+    "http://localhost:" + API_PORT, 
     {
     method: "GET"
     }
